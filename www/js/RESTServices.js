273301ee403d1e971bb8a8d1d8438bdfc278b0f2
ssfmaster <ssfmaster@softstackfactory.org>
2016-04-13 20:48:26 +0000

    Preparing for the app store/android market.

--- www/js/RESTServices.js
+++ www/js/RESTServices.js
@@ -1,7 +1,5 @@
 angular.module('RESTConnection', [])
-.constant('ENDPOINT_URL', 'https://ssf-test-taker-ssfmaster.c9users.io/api/')
-//https://ssf-test-taker-ssfmaster.c9users.io/api/
-//https://apitemperamenttests.softstackfactory.com/api/
+.constant('ENDPOINT_URL', 'https://apitemperamenttests.softstackfactory.com/api/')
 .service('ConUserService', ['$http', 'ENDPOINT_URL', 'SSFConfigConstants',
 function ($http, ENDPOINT_URL, SSFConfigConstants) {
   var service = this;
